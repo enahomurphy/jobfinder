b43a4167c8b0e1db010b7d4771bd1ec0ec1184a7
enaho murphy <enahomurphy@gmail.com>
2016-10-14 16:48:52 +0100

    added the wiredep and inject gulp task

---
+++ app/public/assets/js/app.js
@@ -0,0 +1,17 @@
+angular.module('app', [])
+    .controller('testController', function () {
+
+        this.jobs = [
+            {
+                title: "brewry Nigeria Ltd Manager",
+                description: "While not always necessary, sometimes you need to put your DOM in a box. For those situations, try the panel component."
+            },
+            {
+                title: "Olive variety, sales manage",
+                description: "looking for a very hard working sales rep to work for 10hrs evry day" 
+            }
+        ]
+
+
+
+    })
