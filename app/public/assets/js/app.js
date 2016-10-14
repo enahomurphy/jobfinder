be3520e97a7bf5e7bb740b56a2ba066855d7e335
enaho murphy <enahomurphy@gmail.com>
2016-10-14 20:37:14 +0100

    added the ngResourse and replaced jobs dummy data with data from mongodb

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -1,18 +1,7 @@
-angular.module('app', [])
-    .controller('testController', function ($scope) {
+angular.module('app', ['ngResource'])
+    .controller('testController', function ($scope, $resource) {
 
-        $scope.jobs = [
-            {
-                title: "brewry Nigeria Ltd Manager",
-                description: "While not always necessary, sometimes you need to put your DOM in a box. For those situations, try the panel component."
-            },
-            {
-                title: "Olive variety, sales manage",
-                description: "looking for a very hard working sales rep to work for 10hrs evry day" 
-            }
-        ]
+        $scope.jobs = $resource('/api/jobs').query()
 
-        console.log("hello world from murphy")
-
-
+        console.log($scope.jobs)
     })
