34864685b349bcc7753f978dc06a917a684662ec
enaho murphy <enahomurphy@gmail.com>
2016-10-15 11:49:45 +0100

    refactored the job test

--- app/models/Job.js
+++ app/models/Job.js
@@ -1,4 +1,7 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+mongoose.Promise = Promise
+
 
 var jobSchema = mongoose.Schema({
 
@@ -11,30 +14,36 @@ var jobSchema = mongoose.Schema({
     }
 })
 
-var job = mongoose.model('Job', jobSchema)
+var Job = mongoose.model('Job', jobSchema)
+exports.db = Job
 
-exports.seedJobs = function (callback) {
-    job.find({}).exec(function (err, jobs) {
-        if (jobs.length === 0) {
-            job.create({
-                title: "Saro agrosciences limited Jobs matching your search criteria",
-                description: "Saro AgroSciences Limited is the leader in the Nigerian Crop Protection Industry. We are a wholly indigenous company, committed to the provision of dependable solutions to crop production problems..."
-            })
-            job.create({
-                title: "Social Media Internet Assessor",
-                description: "ionbridge Technologies, Inc. (NASDAQ: LIOX) is a leading provider of globalization and testing services. Lionbridge has a global network of more than 200,000 resources focused on translation and localization services across 100+ countries. "
+exports.allJobs = function() {
+    console.log("dkdkdk")
+    return  Job.find({}).exec()
+}
 
-            })
-            job.create({
-                title: "Head of Industrial Projects",
-                description: "Our client, a leading investment holding company based in Abuja, Nigeria. With over 30 years high-level experience and investment success in several companies , requires for immediate employment"
-            })
-            job.create({
-                title: "Head of Industrial Projects",
-                description: "Bradfield Consulting Limited - Our client is a major player in international freight forwarding business and they wish to fill this position with a suitable candidate   Overall Role.."
-            }, callback)
-        }
+exports.seedJobs = function () {
+    return new Promise(function (resolve, reject) {
+        Job.find({}).exec(function (err, jobs) {
+            if (jobs.length === 0) {
+                Job.create({
+                    title: "Saro agrosciences limited Jobs matching your search criteria",
+                    description: "Saro AgroSciences Limited is the leader in the Nigerian Crop Protection Industry. We are a wholly indigenous company, committed to the provision of dependable solutions to crop production problems..."
+                })
+                Job.create({
+                    title: "Social Media Internet Assessor",
+                    description: "ionbridge Technologies, Inc. (NASDAQ: LIOX) is a leading provider of globalization and testing services. Lionbridge has a global network of more than 200,000 resources focused on translation and localization services across 100+ countries. "
+
+                })
+                Job.create({
+                    title: "Head of Industrial Projects",
+                    description: "Our client, a leading investment holding company based in Abuja, Nigeria. With over 30 years high-level experience and investment success in several companies , requires for immediate employment"
+                })
+                Job.create({
+                    title: "Head of Industrial Projects",
+                    description: "Bradfield Consulting Limited - Our client is a major player in international freight forwarding business and they wish to fill this position with a suitable candidate   Overall Role.."
+                }, resolve)
+            }
+        })
     })
-
-
 }
--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -1,27 +1,31 @@
-var expect = require('chai').expect
-var mongoose = require('mongoose')
-var jobModel = require('../app/models/Job')
-
-mongoose.connect('mongodb://localhost/jobfinder')
-
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+var jobModel = require('../app/models/Job');
+var Promise = require('bluebird');
 
 
-function resetJobs(callback) {
-    mongoose.connection.collections['jobs'].drop(callback)
+mongoose.Promise = Promise;
+var connectDb = Promise.promisify(mongoose.connect, {
+    context: mongoose
+});
+
+
+function resetJobs() {
+    return new Promise(function (resolve, reject) {
+        mongoose.connection.collections['jobs'].drop(resolve, reject)
+    })
 }
 
 describe('get jobs', function () {
-
     it('shuold should always return a job since jobs are seeded', function (done) {
-        resetJobs(function () {
-            jobModel.seedJobs(function () {
-                mongoose.model('Job').find({}).exec(function (err, jobList) {
-                    expect(jobList).to.have.length.of.at.least(2)
-                    console.log(jobList.length, "hello")
-                    done()
-                    console
-                })
+        connectDb('mongodb://localhost/jobfinder')
+            .then(resetJobs())
+            .then(jobModel.seedJobs)
+            .then(jobModel.allJobs)
+            .then(function (jobList) {
+                console.log(jobList)
+                expect(jobList.length).to.be.at.least(1)
+                done()
             })
-        })
     })
 })
